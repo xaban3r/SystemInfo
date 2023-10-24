3fdf2640df25b45ddf3f4a4df3bf8066c9281a0d
Aleksey <nstr3@mail.ru>
2023-10-24 17:38:29 +0500

    all func was added

--- systeminfo.py
+++ systeminfo.py
@@ -1,4 +1,6 @@
 import json
+from platform import uname
+
 import psutil
 
 
@@ -11,48 +13,86 @@ def size_format(size):
 
 
 def get_info():
-    print(psutil.cpu_percent(interval=1))  # текущая загрузка ЦП в масштабах всей системы в процентах.
-    print(psutil.cpu_count())  # количество логических процессоров в системе
-    print(psutil.cpu_count(logical=False))  # количество логических процессоров в системе
-    print(psutil.cpu_freq())
-    print([x / psutil.cpu_count() * 100 for x in
-           psutil.getloadavg()])  # Возвращает среднюю загрузку системы за последние 1, 5 и 15 минут в виде кортежа.
-    """total: total physical memory (exclusive swap).
-        available: the memory that can be given instantly to processes without the system going into swap. This is 
-        calculated by summing different memory metrics that vary depending on the platform. It is supposed to be used to 
-        monitor actual memory usage in a cross platform fashion.
-        percent: the percentage usage calculated as (total - available) / total * 100.
-        Other metrics:
-        used: memory used, calculated differently depending on the platform and designed for informational purposes 
-        only. total - free does not necessarily match used.
-        free: memory not being used at all (zeroed) that is readily available; note that this doesn’t reflect the actual 
-        memory available (use available instead). total - used does not necessarily match free.
-        active (UNIX): memory currently in use or very recently used, and so it is in RAM.
-        inactive (UNIX): memory that is marked as not used.
-        buffers (Linux, BSD): cache for things like file system metadata.
-        cached (Linux, BSD): cache for various things.
-        shared (Linux, BSD): memory that may be simultaneously accessed by multiple processes.
-        slab (Linux): in-kernel data structures cache."""
-    print(psutil.virtual_memory())
-    """total: total swap memory in bytes
-    used: used swap memory in bytes
-    free: free swap memory in bytes
-    percent: the percentage usage calculated as (total - available) / total * 100"""
-    print(psutil.swap_memory())
-    """device: the device path (e.g. "/dev/hda1"). On Windows this is the drive letter (e.g. "C:\\").
-    mountpoint: the mount point path (e.g. "/"). On Windows this is the drive letter (e.g. "C:\\").
-    fstype: the partition filesystem (e.g. "ext3" on UNIX or "NTFS" on Windows).
-    opts: a comma-separated string indicating different mount options for the drive/partition. Platform-dependent.
-    maxfile: the maximum length a file name can have.
-    maxpath: the maximum length a path name (directory name + base file name) can have."""
-    print(psutil.disk_partitions())
-    print(psutil.disk_usage('/'))  # argument - path to disk example: '/', '/home', "C:\\"
-    # Нужно будет иттерироваться по psutil.disk_partitions()
+    ram = psutil.virtual_memory()
+    swap = psutil.swap_memory()
+    cpu_freq = psutil.cpu_freq()
+    system_info = {
+        "System": {
+            "OS version": f"{uname().system} {uname().release}",
+            "PC name": uname().node,
+            "Version": uname().version,
+            'Machine': uname().machine
+        },
+        "CPU": {
+            'name': uname().processor,
+            "Load (%)": psutil.cpu_percent(interval=1),
+            "Logical CPU Count": psutil.cpu_count(),
+            "CPU Count": psutil.cpu_count(logical=False),
+            "CPU frequence (Ghz)": {
+                "Current": int(cpu_freq.current),
+                "Min": int(cpu_freq.min),
+                "Max": int(cpu_freq.max)
+            },
+        },
+        "RAM": {
+            "Memory": {
+                "Total": size_format(ram.total),
+                "Available": size_format(ram.available),
+                "Load": size_format(ram.percent),
+                "Used": size_format(ram.used)
+            },
+            "Swap": {
+                "Total": size_format(swap.total),
+                "Used": size_format(swap.used),
+                "Free": size_format(swap.free)
+            }
+        },
+        "Disk": {}
 
-    print(psutil.sensors_temperatures())
+    }
+
+    for partition in psutil.disk_partitions():
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+
+            if f"'device': {partition.device}" not in system_info["Disk"]:
+                system_info["Disk"][partition.device] = dict()
+                system_info["Disk"][partition.device] = {'File system': partition.fstype,
+                                                         'Total': size_format(
+                                                             partition_usage.total),
+                                                         'Used': size_format(
+                                                             partition_usage.used),
+                                                         'Free': size_format(
+                                                             partition_usage.free),
+                                                         'Percent':
+                                                             f'{partition_usage.percent}'}
+        except Exception as err:
+            print(err)
+            continue
+    return system_info
+
+
+def print_sys_info(system_info):
+    for category, values in system_info.items():
+        print(f"{category}:")
+        if isinstance(values, dict):
+            for key, val in values.items():
+                if isinstance(val, dict):
+                    print(f"  {key}:")
+                    for sub_key, sub_val in val.items():
+                        print(f"    {sub_key}: {sub_val}")
+                else:
+                    print(f"  {key}: {val}")
+        else:
+            print(f"  {values}")
+        print()
+
 
 def main():
-    get_info()
+    info = get_info()
+    with open(f'{uname().node}.json', 'w', encoding='utf-8') as file:
+        json.dump(info, file, indent=4, ensure_ascii=False)
+    print_sys_info(info)
 
 
 if __name__ == "__main__":
